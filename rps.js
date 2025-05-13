ea94ee4560cc23a72918ff171c9d37eb54982eda
Ben Fetterman <bjfett24@gmail.com>
2025-05-13 18:08:08 -0400

    Debug issue with only running game once
    The for-loop syntax was not correct before.

--- rps.js
+++ rps.js
@@ -25,36 +25,6 @@ function getHumanChoice() {
 
 // console.log(getHumanChoice())
 
-let humanScore = 0;
-
-let computerScore = 0;
-
-function playRound(compChoice, humanChoice) {
-    compChoice = compChoice.toLowerCase()
-    humanChoice = humanChoice.toLowerCase()
-    if (compChoice == humanChoice) {
-        console.log(`Draw! ${compChoice} and ${humanChoice} are equal!`);
-    } else if (compChoice == "rock" && humanChoice == "paper") {
-        ++humanScore;
-        console.log("You win! Your paper beats rock.");
-    } else if (compChoice == "paper" && humanChoice == "rock") {
-        ++computerScore;
-        console.log("You lose! Paper beats your rock.");
-    } else if (compChoice == "paper" && humanChoice == "scissors") {
-        ++humanScore;
-        console.log("You win! Your scissors beats paper.");
-    } else if (compChoice == "scissors" && humanChoice == "paper") {
-        ++computerScore;
-        console.log("You lose! Scissors beats your paper.");
-    } else if (compChoice == "rock" && humanChoice == "scissors") {
-        ++computerScore;
-        console.log("You lose! Rock beats your scissors.");
-    } else if (compChoice == "scissors" && humanChoice == "rock") {
-        ++humanScore;
-        console.log("You win! Your rock beats scissors");
-    }
-}
-
 
 
 // playRound(computerSelection, humanSelection);
@@ -70,40 +40,42 @@ function playGame(numOfRounds) {
         if (compChoice == humanChoice) {
             console.log(`Draw! ${compChoice} and ${humanChoice} are equal!`);
         } else if (compChoice == "rock" && humanChoice == "paper") {
-            ++humanScore;
+            humanScore = ++humanScore;
             console.log("You win! Your paper beats rock.");
         } else if (compChoice == "paper" && humanChoice == "rock") {
-            ++computerScore;
+            computerScore = ++computerScore;
             console.log("You lose! Paper beats your rock.");
         } else if (compChoice == "paper" && humanChoice == "scissors") {
-            ++humanScore;
+            humanScore = ++humanScore;
             console.log("You win! Your scissors beats paper.");
         } else if (compChoice == "scissors" && humanChoice == "paper") {
-            ++computerScore;
+            computerScore = ++computerScore;
             console.log("You lose! Scissors beats your paper.");
         } else if (compChoice == "rock" && humanChoice == "scissors") {
-            ++computerScore;
+            computerScore = ++computerScore;
             console.log("You lose! Rock beats your scissors.");
         } else if (compChoice == "scissors" && humanChoice == "rock") {
-            ++humanScore;
+            humanScore = ++humanScore;
             console.log("You win! Your rock beats scissors");
         }
     }
 
     
-    for (let i = 0; i < numOfRounds; i++)
+
+    for (let i = 0; i < numOfRounds; i++) {
     
         humanSelection = getHumanChoice();
         computerSelection = getComputerChoice();
 
         playRound(humanSelection, computerSelection);
+    }
 
     console.log(`Human Score: ${humanScore}, Computer Score: ${computerScore}`)
     return [humanScore, computerScore]
 
-    
 
-    
 
 }
 
+
+playGame(5)
